1aa20218c5e11e73cce3097f67d475251b780831
zhouhao <zhouhao@wpilife.org>
2013-11-04 18:54:15 -0500

    change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://wpilife.org/';
+$config['base_url']	= 'http://zhouhao.wpilife.org/WPILIFE/';
 
 /*
 |--------------------------------------------------------------------------
