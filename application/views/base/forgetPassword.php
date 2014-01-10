015b832e2789b73b18c78ac278ed92d3e4d29e3a
Hao Zhou <hzhou@wpi.edu>
2014-01-09 22:50:43 -0500

    add new folder for view

---
+++ application/views/base/forgetPassword.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $title; ?></title>
+	<?php $this->load->view('includes/import');?>
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/signup.css" type="text/css" />
+</head>
+<body>
+<!-- Wrapper / Start -->
+<div id="wrapper">
+	<?php $this->load->view('includes/header');?>
+	<div id="content">
+	<?php 
+		echo br(2);
+		echo form_open("login/passwordRecovery");
+	?>
+			<table cellpadding="0" cellspacing="0" border="0" id="log_reg">
+				<tr>
+					<td class="form-input-name">E-mail</td>
+					<td class="input"><input type="email" name="email" placeholder="Your E-mail @wpi.edu" autocomplete="off" required="required" /></td>
+				</tr>
+				<tr>
+					<td class="form-input-name"></td>
+					<td><input type="submit" value="Password Reset" /></td>
+				</tr>
+			</table>
+		<?php echo form_close(br(2)); ?>
+
+	</div>
+</div>
+<?php $this->load->view('includes/footer');?>
+</body>
+</html>
